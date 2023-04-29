925aa09cdb117ee878316c3fa209a42fb0c9fe88
Abid <abid0hussain1733@gmail.com>
2023-04-29 16:18:48 +0530

    added font property

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./build/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        outfit: "Outfit, sans-serif",
+      },
+      fontWeight: {
+        olight: "300",
+        onormal: "400",
+        osemibold: "600",
+      },
+    },
   },
   plugins: [],
 };
