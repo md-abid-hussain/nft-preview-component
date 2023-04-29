d7966f407a8aa2fe7c0cf5a9b6cc30d0493f9521
Abid <abid0hussain1733@gmail.com>
2023-04-29 21:40:06 +0530

    configure tailwind config file for custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./build/**/*.html"],
   theme: {
     extend: {
+      maxWidth: {
+        mobile: "350px",
+      },
       fontFamily: {
         outfit: "Outfit, sans-serif",
       },
@@ -13,6 +16,14 @@ module.exports = {
       },
       boxShadow: {
         "3xl": "0 25px 15px 10px #09121e",
+      },
+      colors: {
+        "custom-blue": "#8bacda",
+        "custom-cyan": "#00fff7",
+        "custom-body-blue": "#0d192b",
+        "custom-card-blue": "#14253d",
+        "hr-color": "#2f415b",
+        "fr-link": "#3e52a3",
       },
     },
   },
